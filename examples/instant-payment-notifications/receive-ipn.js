9c0184895878d1919b2514f93f80b4a8975fdad2
Horacio Casatti <horacio.casatti@mercadolibre.com>
2013-05-07 10:14:11 -0300

    recurring

--- examples/instant-payment-notifications/receive-ipn.js
+++ examples/instant-payment-notifications/receive-ipn.js
@@ -4,11 +4,37 @@ var MP = require ("../../project/lib/mercadopago"),
 exports.run = function (req, res) {
     var mp = new MP (config.client_id, config.client_secret);
 
-    mp.getPaymentInfo (req.param("id"), function (err, data){
-        if (err) {
-            res.send (err);
-        } else {
-            res.render ("jsonOutput", {"result": data});
-        }
-    });
+    switch (req.param("topic")) {
+        case "payment":
+            mp.getPayment (req.param("id"), function (err, data){
+                if (err) {
+                    res.send (err);
+                } else {
+                    res.render ("jsonOutput", {"result": data});
+                }
+            });
+            break;
+
+        case "authorized_payment":
+            mp.getAuthorizedPayment (req.param("id"), function (err, data){
+                if (err) {
+                    res.send (err);
+                } else {
+                    res.render ("jsonOutput", {"result": data});
+                }
+            });
+            break;
+
+        case "preapproval":
+            mp.getPreapprovalPayment (req.param("id"), function (err, data){
+                if (err) {
+                    res.send (err);
+                } else {
+                    res.render ("jsonOutput", {"result": data});
+                }
+            });
+            break;
+
+    }
+
 };
