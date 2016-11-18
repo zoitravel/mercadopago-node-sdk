e0ca57a96fd2ac43bfd0a4cfd328682abdab0bb4
Ramon Castillo <jrcastillo200@gmail.com>
2016-11-18 17:30:59 -0400

    removed preferences and preapproval support

--- credentials.js
+++ credentials.js
@@ -2,3 +2,5 @@ require('dotenv').config({silent: true, path: 'sdk-nodejs/.env'});
 
 module.exports.ACCESS_TOKEN = process.env.ACCESS_TOKEN;
 module.exports.PUBLIC_KEY = process.env.PUBLIC_KEY;
+module.exports.CLIENT_ID = process.env.CLIENT_ID;
+module.exports.CLIENT_SECRET = process.env.CLIENT_SECRET;
